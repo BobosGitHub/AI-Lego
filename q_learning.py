0920c1a5addd604095b2727432bada267d61f16f
Axel Wickman <axelwickm@gmail.com>
2018-12-18 19:38:54 +0100

    Added Siraj's code as base for TD-learning

---
+++ q_learning.py
@@ -0,0 +1,88 @@
+"""
+Q-Learning example using OpenAI gym MountainCar enviornment
+Author: Moustafa Alzantot (malzantot@ucla.edu)
+"""
+import numpy as np
+
+import gym
+from gym import wrappers
+
+n_states = 20
+iter_max = 10000
+
+initial_lr = 1.0 # Learning rate
+min_lr = 0.003
+gamma = 1.0
+t_max = 10000
+eps = 0.02
+
+def run_episode(env, policy=None, render=False):
+    obs = env.reset()
+    total_reward = 0
+    step_idx = 0
+    for _ in range(t_max):
+        if render:
+            env.render()
+        if policy is None:
+            action = env.action_space.sample()
+        else:
+            a,b = obs_to_state(env, obs)
+            action = policy[a][b]
+        obs, reward, done, _ = env.step(action)
+        total_reward += gamma ** step_idx * reward
+        step_idx += 1
+        if done:
+            break
+    return total_reward
+
+def obs_to_state(env, obs):
+    """ Maps an observation to state """
+    a = int((obs[1]+2)/4*n_states)
+    b = float((obs[2]+24.0)/48.0)
+    b = ((b-0.5)*50.0)+0.5
+    b = int(b*n_states)
+    print(str(a)+" "+str(b))
+    a = max(0, min(n_states-1, a))
+    b = max(0, min(n_states-1, b))
+    return a, b
+
+if __name__ == '__main__':
+    env_name = 'CartPole-v1'
+    env = gym.make(env_name)
+    env.seed(0)
+    np.random.seed(0)
+    print ('----- using Q Learning -----')
+    q_table = np.zeros((n_states, n_states, 2))
+    for i in range(iter_max):
+        obs = env.reset()
+        total_reward = 0
+        ## eta: learning rate is decreased at each step
+        eta = max(min_lr, initial_lr * (0.85 ** (i//100)))
+        eta = 0.2
+        for j in range(t_max):
+            a, b = obs_to_state(env, obs)
+            if np.random.uniform(0, 1) < eps:
+                action = np.random.choice(env.action_space.n)
+            else:
+                logits = q_table[a][b]
+                logits_exp = np.exp(logits)
+                probs = logits_exp / np.sum(logits_exp)
+                action = np.random.choice(env.action_space.n, p=probs)
+            obs, reward, done, _ = env.step(action)
+            reward = 1-abs(obs[2]/24)
+            env.render()
+            print("Reward: "+str(reward))
+            total_reward += reward
+            # update q table
+            a_, b_ = obs_to_state(env, obs)
+            q_table[a][b][action] = q_table[a][b][action] + eta * (reward + gamma *  np.max(q_table[a_][b_]) - q_table[a][b][action])
+            if done:
+                print("done")
+                break
+        if i % 100 == 0:
+            print('Iteration #%d -- Total reward = %d.' %(i+1, total_reward))
+    solution_policy = np.argmax(q_table, axis=2)
+    solution_policy_scores = [run_episode(env, solution_policy, True) for _ in range(100)]
+    print("Average score of solution = ", np.mean(solution_policy_scores))
+    # Animate it
+    run_episode(env, solution_policy, True)
