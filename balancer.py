b0eebb549cf68b4e9c4c8a7b20c8bf12782f1f1b
Axel Wickman <axelwickm@gmail.com>
2019-03-28 12:50:52 +0100

    Added balancer class

--- balancer.py
+++ balancer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Hello to the world from ev3dev.org'''
 
 import os
 import sys
 import time
+import math
 import ev3dev2
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor.lego import GyroSensor
@@ -40,9 +40,42 @@ def set_font(name):
     '''
     os.system('setfont ' + name)
 
-def get_gyro_angle(sensor):
-    debug_print("Angle: " + str(sensor.angle))
-    debug_print("Rate: " + str(sensor.rate))
+
+""" Learning balancer class """
+class Balancer:
+    def __init__(self, fail_angle, n_states):
+        self.gs = GyroSensor()
+        time.sleep(0.5)
+        c = self.gs.reset()
+        self.starting_angle, self.starting_rate = self.gs.angle_and_rate
+        self.fail_angle = fail_angle
+        self.n_states = n_states
+        #self.q_table = np.zeros((n_states, n_states, 2))
+
+    def observation_to_state(self, angle, rate):
+        a = math.floor(angle / (2 * self.fail_angle) * self.n_states)
+        b = 0
+        return a, b    
+
+    def run_episode(self):
+        # Wait until the robot is upright
+        while 5 < abs(self.starting_angle - self.gs.angle()):
+            print("Waiting "+str(self.starting_angle - self.gs.angle()))
+
+        # Run until the robot isn't balanced
+        while True:
+            angle = self.starting_angle - self.gs.angle()
+            rate = self.starting_rate - self.gs.rate()
+            print("a "+str(self.gs.angle())+"\nR "+str(self.starting_angle))
+            if self.fail_angle < abs(angle) and False:
+                break
+
+            a, b = self.observation_to_state(angle, rate)
+        
+
+
+
+
 
 def main():
     '''The main function of our program'''
@@ -50,21 +83,23 @@ def main():
     # set the console just how we want it
     reset_console()
     set_cursor(OFF)
-    set_font('Lat15-Terminus24x12')
+    set_font('Lat15-Terminus24x12'),
+
+    # Create balancer
+    balancer = Balancer(45, 20)
+
+    for i in range(100):
+        balancer.run_episode()
 
     # print something to the screen of the device
     print('ALL YOUR BASE ARE BELONG TO US')
-    gs = GyroSensor()
     
-    while True:
-        get_gyro_angle(gs)
-        time.sleep(0.5)
     # print something to the output panel in VS Code
     debug_print('Hello VS Code!')
 
     # wait a bit so you have time to look at the display before the program
     # exits
-    time.sleep(5)
+    time.sleep(1)
 
 if __name__ == '__main__':
     main()
